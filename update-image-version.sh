b40f2b7ec3c3eab2c70733656c453c17d0ed39cb
Jordan Reyes Leger <jordan.leger@hotmail.com>
2024-12-27 08:41:15 -0600

    Adding replacement task

---
+++ update-image-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables
+IMAGE_VERSION=$1
+DEPLOYMENT_FILE="aks-deployment.yaml"
+
+# Reemplazar la versión de la imagen en el archivo YAML
+sed -i "s|image: akscontenedoresdev02.azurecr.io/pasteleriaapi:.*|image: akscontenedoresdev02.azurecr.io/pasteleriaapi:${IMAGE_VERSION}|g" $DEPLOYMENT_FILE
+sed -i "s|image: akscontenedoresdev02.azurecr.io/pasteleriportal:.*|image: akscontenedoresdev02.azurecr.io/pasteleriportal:${IMAGE_VERSION}|g" $DEPLOYMENT_FILE
+
+echo "Updated image versions to ${IMAGE_VERSION} in ${DEPLOYMENT_FILE}"
